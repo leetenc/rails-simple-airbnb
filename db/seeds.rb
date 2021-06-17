49582bd17ecc71ea50e2b091fd837d0df34aa3d1
Leeten Chin <lchin@imsystems.com.au>
2021-06-17 20:52:53 +1000

    Help

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.destroy_all
+
+10.times do
+  Flat.create!(
+    name: Faker::Marketing.buzzwords,
+    address: Faker::Address.full_address,
+    description: Faker::Restaurant.review,
+    price_per_night: rand(50..500),
+    number_of_guests: rand(1..10)
+  )
+end
